03038aabcf3e9bc1acce0a7fff39f55868767ab3
kranthi kumar <kranthi.8t7@gmail.com>
2019-05-28 20:02:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
-function theBeatlesPlay(musicians, instruments) {
+const singers = ["John Lennon", "Ringo Star", "Paul McCartney", "Dizzy"];
+const instruments = ["guitar", "drums", "piano", "trumpet"];
 
-     var allMusicians = [];
 
-     for (var i=0; i < musicians.length; i++) {
-         allMusicians.push(musicians[i]  + " plays " + instruments[i]);
-     }
-     return allMusicians;
- }
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-// add solution here
+ var theBeatlesPlay = function (musiciansArray, instrumentsArray) {
+	var collect = [];
+	for (var i = 0; i<musiciansArray.length; i++) {
+	collect.push(musiciansArray[i] + " plays " + instrumentsArray[i]);
+	}
+	return(collect);
+};
+
+ theBeatlesPlay(singers, instruments);
+
+ var facts = [
+	"He was the last Beatle to learn to drive",
+	"He was never a vegetarian",
+	"He was a choir boy and boy scout",
+	"He hated the sound of his own voice"
+];
+
+ var johnLennonFacts = function (factsArray) {
+	var i = 0;
+	var arrayCounter = factsArray.length
+	var newFactsArray = [];
+
+ 	while (arrayCounter > 0) {
+		newFactsArray.push(factsArray[i] + "!!!");
+		i++;
+		arrayCounter--;
+	}
+	return(newFactsArray);
+};
+
+ johnLennonFacts(facts);
+
+ var iLoveTheBeatles = function (num){
+	var arr = [];
+	num++;
+
+ 	if (num === 18) {
+		return("I love the Beatles!");
+	}
+
+ 	var i = 0;
+	do {
+		arr.push("I love the Beatles!");
+		i++;
+
+ 	} while (i < num);
+
+ 	return(arr);
+}
+
+ iLoveTheBeatles(2);
