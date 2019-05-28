cd16f81b70966197accaa7274da6980686730265
kranthi kumar <kranthi.8t7@gmail.com>
2019-05-28 20:00:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function theBeatlesPlay(musicians, instruments) {
+  
+     var allMusicians = [];
+     
+     for (var i=0; i < musicians.length; i++) {
+         allMusicians.push(musicians[i]  + " plays " + instruments[i]);
+     }
+     return allMusicians;
+ }
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]; 
 // add solution here
